f63cff568bbe7ef08e95d2f34d6eb4790fa03cd5
thomasabigkat <thomas@thomasm.com>
2014-11-20 22:31:38 -0500

    fad

--- dshealthcheck.bat
+++ dshealthcheck.bat
@@ -2,11 +2,10 @@ dscheck /full servers /silent > c:\support\dscheck.txt
 dscheck /full apps /silent >> c:\support\dscheck.txt
 rem dscheck /full printers OPTIONS /silent >> c:\support\dscheck.txt
 dscheck /full groups /silent >> c:\support\dscheck.txt
-dscheck /full mslicense /list /silent >> c:\support\dscheck.txt
-dscheck /full folders /silent >> c:\support\dscheck.txt
-dscheck /full licenses /silent >>  c:\support\dscheck.tx
-dscheck /full installationjobs /silent >> c:\support\dscheck.txt
-dscheck /full fta /duplicates /silent >> c:\support\dscheck.txt
-dscheck /full hotfixfolder /silent >> c:\support\dscheck.txt
+
+fuck fuck fuck a duck
+
+
+
 dscheck /full rmserver /silent >> c:\support\dscheck.txt
 dscheck /full admintools /silent >> c:\support\dscheck.txt
